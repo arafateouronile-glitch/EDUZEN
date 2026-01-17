de7bb87cb0248dd758f0bfe7658a13cfd9b81f73
arafateouronile-glitch <arafateouronile@gmail.com>
2026-01-18 00:16:40 +0100

    fix: Ajouter props onApplyStyle et onClose à StylePalette
    Le composant StylePalette était utilisé avec les props onApplyStyle et onClose
    mais ne les acceptait pas, causant une erreur TypeScript.
    Ajouter l'interface StylePaletteProps avec ces props optionnelles.

--- components/document-editor/style-palette.tsx
+++ components/document-editor/style-palette.tsx
@@ -1,6 +1,25 @@
 'use client'
 
 // TODO: Implémenter StylePalette
-export function StylePalette() {
+
+interface StylePaletteProps {
+  onApplyStyle?: (style: {
+    fontSize?: string
+    fontWeight?: string
+    fontStyle?: string
+    color?: string
+    backgroundColor?: string
+    textAlign?: string
+    lineHeight?: string
+    marginTop?: string
+    marginBottom?: string
+    padding?: string
+    borderLeft?: string
+    borderColor?: string
+  }) => void
+  onClose?: () => void
+}
+
+export function StylePalette(props?: StylePaletteProps) {
   return null
 }
